2039ea936ffd8684bc7aa1421e26bc98960d4a9d
ipiyou <tao710803@gmail.com>
2023-03-09 12:18:47 +0900

    (#7) feat: 불필요한 속성 제거

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -5,38 +5,29 @@ import { keyOfColor, keyOfFont } from '../../styles/theme';
 interface PropsType {
   className?: string;
   as: keyof JSX.IntrinsicElements;
-  lineHeight?: string;
   height?: string;
   align?: 'start' | 'center' | 'end';
   size?: keyOfFont;
   color?: keyOfColor;
-  margin?: string;
-  padding?: string;
   children: ReactNode;
 }
 export const Text = ({
   className,
   as,
-  lineHeight,
   height,
   align,
   size,
   color,
-  margin,
-  padding,
   children,
 }: PropsType) => {
   return (
     <_Wrapper
       className={className}
       as={as}
-      lineHeight={lineHeight}
       height={height}
       align={align}
       size={size}
       color={color}
-      margin={margin}
-      padding={padding}
     >
       {children}
     </_Wrapper>
@@ -44,12 +35,9 @@ export const Text = ({
 };
 
 const _Wrapper = styled.div<PropsType>`
-  line-height: ${({ lineHeight }) => lineHeight};
   height: ${({ height }) => height};
-  padding: ${({ padding }) => padding && padding};
   text-align: ${({ align }) => align};
   ${({ theme, size }) => theme.font[size]};
 
   color: ${({ theme, color }) => theme.color[color]};
-  margin: ${({ margin }) => margin && margin};
 `;
