d11a2d227ae8dd09501f3170e3d3a86f5aeb1aad
ipiyou <tao710803@gmail.com>
2023-03-09 23:23:04 +0900

    (#9) feat: input 내보내기

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './styles';
 export * from './styles/animation';
 export * from './styles/theme';
 export * from './styles/media';
+export * from './components/text/index'
+export * from './components/input/index'
