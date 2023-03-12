723544cd71c057ba9aba2b85c201572ddf81264f
Kim Tae Wan <82878304+ipiyou@users.noreply.github.com>
2023-03-12 11:38:42 +0900

    (#13) fix: text 요소 props 수정 (#14)
    * (#13) fix: optional chaining props 수정
    
    * (#13) feat: 테케 추가
    
    * (#13) fix: 필요없는 요소, styled 함수 리턴
    
    * (#13) feat: 기본 값 추가
    
    * (#13) fix: size,color optional
    
    * (#13) refactor: text size stories map함수로 타이핑 줄임
    
    ---------
    
    Co-authored-by: Josanghyeon <BStkdgus.dev@gmail.com>

--- src/components/text/text.stories.tsx
+++ src/components/text/text.stories.tsx
@@ -1,94 +1,85 @@
 import React from 'react';
 import { Text } from './index';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import styled from 'styled-components';
+import { font } from '../../styles/theme/font';
+
+const fontList = Object.keys(font).map((k) => font[k]);
 
 export default {
   title: 'component/text',
   component: Text,
 } as ComponentMeta<typeof Text>;
 
-const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
-
-export const contain1 = Template.bind({});
-
-contain1.args = {
-  size: 'title1',
-  children: '안녕하세요 여러분들 반갑습니다.',
+export const Size: ComponentStory<typeof Text> = () => {
+  return (
+    <_Wrapper>
+      {fontList.map((font) => (
+        <Text size={font} key={font} color="gray700">
+          안녕하세요 여러분들 반갑습니다.
+        </Text>
+      ))}
+    </_Wrapper>
+  );
 };
 
-export const contain2 = Template.bind({});
-
-contain2.args = {
-  size: 'title2',
-  children: '안녕하세요 여러분들 반갑습니다.',
+export const Color = () => {
+  return (
+    <_Wrapper>
+      <Text size="title2" color="gray700">
+        안녕하세요 여러분들 반갑습니다.
+      </Text>
+      <Text size="title2" color="primary700">
+        안녕하세요 여러분들 반갑습니다.
+      </Text>
+      <Text size="title2" color="error700">
+        안녕하세요 여러분들 반갑습니다.
+      </Text>
+      <Text size="title2" color="warning700">
+        안녕하세요 여러분들 반갑습니다.
+      </Text>
+      <Text size="title2" color="success700">
+        안녕하세요 여러분들 반갑습니다.
+      </Text>
+    </_Wrapper>
+  );
 };
 
-export const contain3 = Template.bind({});
+const alignExample = `토끼, 않은 이런 계절이 내일 하나에 별빛이 이네들은 봅니다. 이름과, 멀리 내 있습니다. 별에도 별 이름과, 이제 언덕 프랑시스 소학교 까닭입니다. 잔디가 소학교 둘 패, 어머니, 그리워 애기 듯합니다. 소녀들의 내일 노루, 별 가을 옥 이름과 내 있습니다. 같이 나는 오는 패, 이름과 새겨지는 둘 사람들의 듯합니다. 비둘기, 새겨지는 별 우는 않은 까닭입니다.`;
 
-contain3.args = {
-  size: 'title3',
-  children: '안녕하세요 여러분들 반갑습니다.',
+export const Align = () => {
+  return (
+    <_Wrapper>
+      <div>
+        <Text size="title2" color="gray700">
+          Start
+        </Text>
+        <Text size="body1" color="gray700" align="start">
+          {alignExample}
+        </Text>
+      </div>
+      <div>
+        <Text size="title2" color="gray700">
+          Center
+        </Text>
+        <Text size="body1" color="gray700" align="center">
+          {alignExample}
+        </Text>
+      </div>
+      <div>
+        <Text size="title2" color="gray700">
+          End
+        </Text>
+        <Text size="body1" color="gray700" align="end">
+          {alignExample}
+        </Text>
+      </div>
+    </_Wrapper>
+  );
 };
 
-export const contain4 = Template.bind({});
-
-contain4.args = {
-  size: 'title4',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain5 = Template.bind({});
-
-contain5.args = {
-  size: 'body1',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain6 = Template.bind({});
-
-contain6.args = {
-  size: 'body2',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain7 = Template.bind({});
-
-contain7.args = {
-  size: 'body3',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain8 = Template.bind({});
-
-contain8.args = {
-  size: 'body4',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain9 = Template.bind({});
-
-contain9.args = {
-  size: 'heading1',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain10 = Template.bind({});
-
-contain10.args = {
-  size: 'heading2',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain11 = Template.bind({});
-
-contain11.args = {
-  size: 'heading3',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
-
-export const contain12 = Template.bind({});
-
-contain12.args = {
-  size: 'heading4',
-  children: '안녕하세요 여러분들 반갑습니다.',
-};
+const _Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
