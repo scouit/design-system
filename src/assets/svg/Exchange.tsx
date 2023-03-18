cac356c025fb5f95e282216125af6ee3620d3f03
ipiyou <tao710803@gmail.com>
2023-03-18 20:55:26 +0900

    (#17) fix: xmlns 제거

--- src/assets/svg/Exchange.tsx
+++ src/assets/svg/Exchange.tsx
@@ -4,7 +4,6 @@ export const Exchange = () => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    xmlns="http://www.w3.org/2000/svg"
   >
     <path
       d="M21 9L17 5V8H10V10H17V13M7 11L3 15L7 19V16H14V14H7V11Z"
