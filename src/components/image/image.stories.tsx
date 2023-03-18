c1443d354069a0ec18ed0e453768916b152a02d7
ipiyou <tao710803@gmail.com>
2023-03-18 18:06:46 +0900

    (#17) feat: 테케 추가

--- src/components/image/image.stories.tsx
+++ src/components/image/image.stories.tsx
@@ -4,14 +4,27 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ChangeEvent } from 'react';
 import { Image } from '../../assets/svg';
 import { ClickImg } from '../../assets/imgs';
+import {
+  ComponentStoryType,
+  ObjectExclude,
+  Parameter,
+  templateBindRequireAllArgs,
+} from '../../types/story';
 
 export default {
   title: 'component/image',
   component: ImageInput,
 } as ComponentMeta<typeof ImageInput>;
 
-export const Template: ComponentStory<typeof ImageInput> = () => {
-  const [state, setState] = useState<string[]>([]);
+type DefaultProps = ObjectExclude<
+  Parameter<typeof ImageInput>,
+  'label' | 'imageList' | 'imgToUrl' | 'onChagne'
+>;
+
+const Template: ComponentStoryType<DefaultProps> = (arg) => {
+  const [state, setState] = useState<string[]>([
+    'https://storage.surfit.io/career/portfolio/cover/wo9NR/22355437964143740239c1.jpg',
+  ]);
 
   const onChange = (value: string[]) => {
     setState(value);
@@ -26,11 +39,17 @@ export const Template: ComponentStory<typeof ImageInput> = () => {
 
   return (
     <ImageInput
+      {...arg}
       label="이미지 (최대 10개)"
       imageList={state}
       imgToUrl={imgToUrl}
-      isLoading={false}
       onChagne={onChange}
     />
   );
 };
+
+const templateBind = templateBindRequireAllArgs(Template);
+
+export const standard = templateBind({ isLoading: false });
+
+export const loading = templateBind({ isLoading: true });
