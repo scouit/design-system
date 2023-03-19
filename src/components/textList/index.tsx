701000eea38689ca6e5e6bb02f36b2623c42eddd
ipiyou <tao710803@gmail.com>
2023-03-19 17:23:14 +0900

    (#29) feat: placeholder 추가, 버튼 css 변경

--- src/components/textList/index.tsx
+++ src/components/textList/index.tsx
@@ -5,9 +5,10 @@ import { Plus, TrashCan } from '../../assets/svg';
 interface PropsType {
   textList: string[];
   onChange: (value: string[]) => void;
+  placeholder: string;
 }
 
-export const TextList = ({ textList, onChange }: PropsType) => {
+export const TextList = ({ textList, onChange, placeholder }: PropsType) => {
   const onItemChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
     const temp = [...textList];
@@ -26,7 +27,12 @@ export const TextList = ({ textList, onChange }: PropsType) => {
     <_Wrapper>
       {textList.map((text, index) => (
         <_TextItem>
-          <_Input value={text} name={String(index)} onChange={onItemChange} />
+          <_Input
+            value={text}
+            name={String(index)}
+            onChange={onItemChange}
+            placeholder={placeholder}
+          />
           <_Remove onClick={() => RemoveItem(index)}>
             <TrashCan />
           </_Remove>
@@ -55,29 +61,31 @@ const _Input = styled.input`
   width: 100%;
   flex: 1;
   border: 1px solid ${({ theme }) => theme.color.gray500};
-  color:  ${({ theme }) => theme.color.gray500};
+  color: ${({ theme }) => theme.color.gray500};
   padding: 0 16px;
   border-radius: ${({ theme }) => theme.borderRadius.small};
   ${({ theme }) => theme.font.heading3};
 `;
 
-const _Remove = styled.div`
-  cursor: pointer;
-  width: 38px;
+const _Add = styled.div`
+  width: calc(100% - 48px);
   height: 38px;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: ${({ theme }) => theme.borderRadius.small};
-  :hover {
-    background-color: ${({ theme }) => theme.color.gray100};
-  }
-`;
-
-const _Add = styled(_Remove)`
-  width: calc(100% - 48px);
   background-color: ${({ theme }) => theme.color.primary200};
   :hover {
     background-color: ${({ theme }) => theme.color.primary300};
+  }
+`;
+
+const _Remove = styled(_Add)`
+  cursor: pointer;
+  width: 38px;
+  border: 1px solid ${({ theme }) => theme.color.gray500};
+  background-color: ${({ theme }) => theme.color.gray25};
+  :hover {
+    background-color: ${({ theme }) => theme.color.gray100};
   }
 `;
