0a870b60b4723a651a70b6308004b018873e2f2d
Kim Tae Wan <82878304+ipiyou@users.noreply.github.com>
2023-03-20 13:47:38 +0900

    (#17) feat: 이미지 input 요소 구현 (#18)
    * (#17) feat: 현재 작업 커밋
    
    * (#17) feat: 퍼블리싱
    
    * (#17) feat: css 오류, onChagne 수정
    
    * (#17) feat: 로딩 추가,api 관련 props 추가
    
    * (#17) feat: 이미지 삭제 추가
    
    * (#17) feat: 커서 포인트 추가
    
    * (#17) fix: 네이밍 변경
    
    * (#17) fix: 마진 값 변경
    
    * (#17) feat: 테케 추가
    
    * (#17) fix: 추가 안되는 버그 해결
    
    * (#17) feat: 마진 위치 변경
    
    * (#17) fix: hover background의 radius크기 조정
    
    * (#17) feat: 파일 제한 추가
    
    * (#17) fix: xmlns 제거
    
    * (#17) fix: 필요없는 얕은 복사
    
    * (#17) fix: clickedIndex -> clickedImage
    
    * (#17) fix: 테스트용 promise 생성
    
    * (#17) fix: 필요없는 css 제거
    
    * (#17) fix: 인라인 함수 제거
    
    * (#17) feat: export 추가
    
    * (#17) fix: 이미지 인식 오류 webpack 수정
    
    * (#17) fix: file-loader 추가

---
+++ src/assets/svg/Exchange.tsx
@@ -0,0 +1,13 @@
+export const Exchange = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M21 9L17 5V8H10V10H17V13M7 11L3 15L7 19V16H14V14H7V11Z"
+      fill="#555555"
+    />
+  </svg>
+);
---
+++ src/components/image/index.tsx
@@ -0,0 +1,216 @@
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { Loading } from '../../assets/gif';
+import { ClickImg } from '../../assets/imgs';
+import { Exchange, Image, Trash } from '../../assets/svg';
+import { Text } from '../text';
+
+const buttonRadius = {
+  left: '100px 0 0 100px',
+  right: '0 100px 100px 0',
+};
+
+interface PropsType {
+  label: string;
+  isLoading: boolean;
+  imageList: string[];
+  imgToUrl: (value: File) => Promise<string>;
+  onChagne: (value: string[]) => void;
+}
+
+export const ImageInput = ({
+  label,
+  imageList,
+  isLoading,
+  imgToUrl,
+  onChagne,
+}: PropsType) => {
+  const fileRef = useRef<HTMLInputElement | null>(null);
+  const [clickedImage, setImageIndex] = useState<number | null>(null);
+
+  const onListItemClick = (itemIdx?: number) => {
+    if (isLoading) return;
+    if (itemIdx !== undefined) setImageIndex(itemIdx);
+    fileRef.current.click();
+  };
+
+  const onAddImgClick = () => onListItemClick();
+
+  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files[0];
+    const render = new FileReader();
+    render.readAsDataURL(file);
+    render.onloadend = async () => {
+      const resultURL = await imgToUrl(file);
+      onChagne(fileChangeOrAdd(resultURL));
+      setImageIndex(null);
+    };
+  };
+
+  const fileChangeOrAdd = (value: string) => {
+    if (clickedImage !== null)
+      return imageList.map((e, idx) => (idx === clickedImage ? value : e));
+    return imageList.concat(value);
+  };
+
+  const fileRemove = (itemIdx: number) => {
+    const filteredList = imageList.filter((_, idx) => itemIdx !== idx);
+    onChagne(filteredList);
+  };
+
+  return (
+    <>
+      <_Label size="title2" color="gray500">
+        {label}
+      </_Label>
+      <_Wrapper>
+        <_ImgInput
+          id="imgInput"
+          type="file"
+          onChange={onFileChange}
+          ref={fileRef}
+          accept=".gif, .jpg, .png"
+        />
+        <_ImageList>
+          {imageList.map((img, itemIdx) => (
+            <_ImageItem>
+              <_Img src={img} />
+              <_ImageItemBackground />
+              <_ImageItemActive>
+                <_ImageItemActiveButton
+                  kind="left"
+                  onClick={() => onListItemClick(itemIdx)}
+                >
+                  <Exchange />
+                </_ImageItemActiveButton>
+                <_ImageItemActiveButton
+                  kind="right"
+                  onClick={() => fileRemove(itemIdx)}
+                >
+                  <Trash />
+                </_ImageItemActiveButton>
+              </_ImageItemActive>
+            </_ImageItem>
+          ))}
+        </_ImageList>
+        <div onClick={onAddImgClick}>
+          {imageList.length ? (
+            <_AddImgWrapper>
+              {isLoading ? (
+                <img width={24} height={24} src={Loading} />
+              ) : (
+                <Image />
+              )}
+            </_AddImgWrapper>
+          ) : (
+            <_AddImg src={ClickImg} />
+          )}
+        </div>
+      </_Wrapper>
+    </>
+  );
+};
+
+const _Wrapper = styled.div`
+  display: flex;
+`;
+
+const _ImageList = styled.div`
+  max-width: 100%;
+  display: flex;
+  gap: 20px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  ::-webkit-scrollbar {
+    height: 5px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.color.primary500};
+    border-radius: ${({ theme }) => theme.borderRadius.circle};
+  }
+`;
+
+const _AddImg = styled.img`
+  cursor: pointer;
+  width: 250px;
+  height: 125px;
+  object-fit: cover;
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+`;
+
+const _Img = styled(_AddImg)`
+  cursor: move;
+  border: 1px solid ${({ theme }) => theme.color.gray300};
+`;
+
+const _ImageItemBackground = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  background-color: ${({ theme }) => theme.color.gray900};
+  opacity: 30%;
+  top: 0;
+`;
+
+const _ImageItemActive = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const _ImageItemActiveButton = styled.button<{ kind: 'left' | 'right' }>`
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${({ kind }) => buttonRadius[kind]};
+  background-color: ${({ theme }) => theme.color.gray25};
+  :hover {
+    background-color: ${({ theme }) => theme.color.gray100};
+  }
+`;
+
+const _ImageItem = styled.div`
+  position: relative;
+  height: 125px;
+  margin-bottom: 10px;
+  ${_ImageItemBackground}, ${_ImageItemActive} {
+    visibility: hidden;
+  }
+  :hover {
+    ${_ImageItemBackground}, ${_ImageItemActive} {
+      visibility: visible;
+    }
+  }
+`;
+
+const _ImgInput = styled.input`
+  display: none;
+`;
+
+const _AddImgWrapper = styled.div`
+  cursor: pointer;
+  width: 30px;
+  height: 125px;
+  margin-left: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  background-color: ${({ theme }) => theme.color.gray300};
+`;
+
+const _Label = styled(Text)`
+  margin-bottom: 21px;
+`;
