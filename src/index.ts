74f24c42e195274ae1d10bce21c021f6e21e65d8
ipiyou <tao710803@gmail.com>
2023-03-20 13:52:39 +0900

    (#29) fix: export conflict 해결

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export * from './components/image';
 export * from './components/textarea/index';
 export * from './components/button/index';
 export * from './components/date';
+export * from './components/textList'
